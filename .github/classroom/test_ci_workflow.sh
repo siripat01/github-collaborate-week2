0195ca42665040603959b9f7d9bad790346eebcf
siripat01 <siriboonruangsiripat@gmail.com>
2025-11-27 11:11:27 +0700

    fix file name

--- .github/classroom/test_ci_workflow.sh
+++ .github/classroom/test_ci_workflow.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Check if CI workflow file exists
-if [[ -f ".github/workflows/ci.yml" ]]; then
+if [[ -f ".github/workflows/hello-ci.yml" ]]; then
   echo "PASS: CI workflow file exists"
   exit 0
 else
